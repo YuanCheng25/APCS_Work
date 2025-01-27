5d15122768236c2da930999570d926b101ec080f
YuanCheng25 <112510772+YuanCheng25@users.noreply.github.com>
2025-01-26 18:21:48 -0800

    prjwrk

---
+++ Project/Game.java
@@ -0,0 +1,43 @@
+import processing.core.*;
+import java.util.*;
+
+
+public class Game extends PApplet
+{
+    public void settings()
+    {
+        size(1050, 562);
+    }
+
+    public void setup()
+    {
+        scenes = new ArrayList<Scene>(); 
+        scenes.add(new Scene_Start(this));
+    //    scenes.add(new Scene_Play(this));
+      //  scenes.add(new Scene_End(this));
+
+        current = 0;
+    }
+
+    public void draw()
+    {
+        background(0);
+
+        scenes.get(current).display();
+    }
+    
+    public void keyPressed()
+    {
+       current++;
+       if (current >= scenes.size())
+           current = 0;
+    }
+
+    private ArrayList<Scene> scenes;
+    private int current;
+
+    public static void main(String[] args)
+    {
+        PApplet.main("Game");
+    }
+}
---
+++ Project/Scene_Start.java
@@ -0,0 +1,40 @@
+import processing.core.*;
+
+public class Scene_Start implements Scene
+{
+    private PApplet p;
+    private float bWidth=200;
+    private float bHeight=50;
+    private float bX;  
+    private float bY;
+    private PImage backgroundImage;
+    private PFont customFont;
+        
+    public Scene_Start(PApplet p)
+    {
+        this.p = p;
+        bX=p.width/2-bWidth/2;
+        bY=p.height/2-bHeight/2;
+        backgroundImage = p.loadImage("aib.jpg");
+        
+        customFont = p.createFont("slkscr.ttf", 32); 
+    }
+
+    public void display()
+    {
+        p.image(backgroundImage, 0, 0, p.width, p.height);
+        
+        p.fill(41,61,61);
+        p.rect(bX, bY, bWidth, bHeight);
+        
+        p.textFont(customFont);
+
+        p.fill(255,255,255);
+        p.textAlign(PApplet.CENTER);
+        p.textSize(85);
+        p.text("The Average Game", p.width/2, p.height/3);
+        p.textSize(32);
+        p.text("Start", p.width/2, p.height/2);
+    }
+    
+}
